8242088b4995c8a006899d4bc9323f230876edd4
frommars1992 <E:\Users\Zero-cool\AppData\Roaming\The Bat!>
2013-09-12 17:39:04 +0300

    create_soundtrack_by_pressing_#D#

---
+++ Assets/Eat_Em_All/Scripts/Game/PlaySoundtrack.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlaySoundtrack : MonoBehaviour {
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.D))
+			PlayPause();
+	}
+	void PlayPause(){
+		if(audio.isPlaying)
+			audio.Pause();
+		else audio.Play();
+	}
+}
