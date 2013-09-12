a617fb631ac17b9170005d7bd180c075c21069f1
ArchangeL <mc.archangel@mail.ru>
2013-09-12 14:10:49 +0300

    delete unused files from project

--- Eat_Em_All/Assets/Eat_Em_All/Scripts/Game/MovingInsects.cs
+++ Eat_Em_All/Assets/Eat_Em_All/Scripts/Game/MovingInsects.cs
@@ -4,17 +4,6 @@ namespace Assets.Eat_Em_All.Scripts.Game
 {
     public class MovingInsects : MonoBehaviour
     {
-        public GameObject[] StartFromArr = new GameObject[1];
-        public GameObject[] EndToArr = new GameObject[1];
-        public GameObject[] MovingObjectsArr = new GameObject[1];
-        public float speed = 1;
-        private Vector3[] _moveToCoords;
-
-        private void Start()
-        {
-            _moveToCoords = getEndCordinates(StartFromArr);
-        }
-
         private void Update()
         {
             if (Input.GetKeyUp(KeyCode.Space))
@@ -23,25 +12,9 @@ namespace Assets.Eat_Em_All.Scripts.Game
             }
         }
 
-        private Vector3[] getEndCordinates(GameObject[] endObjects)
-        {
-            Vector3[] coords = new Vector3[endObjects.Length];
-
-            for (byte i = 0; i < coords.Length; i++)
-            {
-                coords[i] = endObjects[i].transform.position;
-            }
-
-            return coords;
-        }
-
         private void Move()
         {
-            for (byte i = 0; i < _moveToCoords.Length; i++)
-            {
-                MovingObjectsArr[i].transform.position = new Vector3(_moveToCoords[i].x*Time.deltaTime*speed,
-                    _moveToCoords[i].y*Time.deltaTime*speed, MovingObjectsArr[i].transform.position.z);
-            }
+            transform.Translate(Vector3.forward);
         }
     }
 }
