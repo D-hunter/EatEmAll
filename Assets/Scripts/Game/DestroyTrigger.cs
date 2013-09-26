b7692c395576359d26286aab03238f8baa72377f
frommars1992 <voronov1992@gmail.com>
2013-09-26 19:37:18 +0300

    CompleteEat Script

--- Assets/Scripts/Game/DestroyTrigger.cs
+++ Assets/Scripts/Game/DestroyTrigger.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 namespace Assets.Scripts.Game{
-public class DestroyTrigger : MonoBehaviour {
-	void OnTriggerEnter(Collider other){
-		Debug.Log("Something goes wrong");
-		Destroy(other.gameObject);
-		var score = other.gameObject.GetComponent<InsectInfo>().OnDestroyScoreAdd;
-		var satiety = other.gameObject.GetComponent<InsectInfo>().OnDestroySatietySub;
-		ScoreAndSatiety.Scores += score;
-		ScoreAndSatiety.Satiety += satiety;
+	public class DestroyTrigger : MonoBehaviour {
+		void OnTriggerEnter(Collider other){
+			Debug.Log("Something goes wrong");
+			Destroy(other.gameObject);
+			var score = other.gameObject.GetComponent<InsectInfo>().OnDestroyScoreAdd;
+			var satiety = other.gameObject.GetComponent<InsectInfo>().OnDestroySatietySub;
+			ScoreAndSatiety.Scores += score;
+			ScoreAndSatiety.Satiety += satiety;
+		}
 	}
-}
 }
---
+++ Assets/Scripts/Game/EatInsect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Game{
+	public class EatInsect : MonoBehaviour {
+		void OnTriggerEnter(Collider insect)
+		{
+			Debug.Log("Fasta!!");
+			var score = insect.gameObject.GetComponent<InsectInfo>().OnEatScoreAdd;
+			var satiety = insect.gameObject.GetComponent<InsectInfo>().OnEatSatietyAdd;
+			ScoreAndSatiety.Scores+=score;
+			ScoreAndSatiety.Satiety+=satiety;
+			Destroy(insect.gameObject);
+		}	
+	}
+}
