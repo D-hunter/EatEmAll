313f4dbae031254918318a9747dd45f51b2f9b41
ArchangeL <dhunter24091992@gmail.com>
2013-10-17 19:59:48 +0300

    before shifting the left corner to the 0,0,0

---
+++ Assets/Scripts/Game/ObjSizeCalculator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class ObjSizeCalculator : MonoBehaviour
+    {
+//        public GameObject[] SpawnPoints;
+//        public GameObject
+        public float heightPercent;
+        public float widthPercent;
+        
+
+        void Start()
+        {
+            
+
+        }
+
+        void Update()
+        {
+            CalculatePercents();
+        }
+
+        public void CalculatePercents()
+        {
+            heightPercent = Screen.height * 0.01f;
+            widthPercent = Screen.width * 0.01f;
+//            Debug.Log("Height percent = " + heightPercent + " total height = " + Screen.height);
+//            Debug.Log("Width percent = " + widthPercent + " total width = " + Screen.width);
+        }
+    }
+}
