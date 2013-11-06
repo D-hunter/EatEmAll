49cb3c77e561357f0d20f6a62b551735882bd088
frommars1992 <voronov1992@gmail.com>
2013-11-06 23:48:19 +0200

    melkie_popravki

--- Assets/Scripts/Game/PauseController.cs
+++ Assets/Scripts/Game/PauseController.cs
@@ -4,14 +4,6 @@ using System.Collections;
 namespace Assets.Scripts.Game
 {
 	public class PauseController : MonoBehaviour {
-	
-		bool IsPaused = false;
-		// Use this for initialization
-		void Start () {
-
-		}
-		
-		// Update is called once per frame
 		void Update () 
 		{
 			if(Input.GetKeyUp(KeyCode.Escape))
