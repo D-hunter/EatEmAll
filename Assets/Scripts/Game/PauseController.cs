8a30338b22628e82474f846560009e46b9343ae1
frommars1992 <voronov1992@gmail.com>
2013-11-06 23:26:11 +0200

    added_game_pause

---
+++ Assets/Scripts/Game/PauseController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Game
+{
+	public class PauseController : MonoBehaviour {
+	
+		bool IsPaused = false;
+		// Use this for initialization
+		void Start () {
+
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			if(Input.GetKeyUp(KeyCode.Escape))
+			{
+				if(!GameMenuButtonsController._isGameMenuActive)
+				{
+					GameMenuButtonsController.PushGameMenuButton();
+				}
+				else
+				{
+					GameMenuButtonsController.PushResumeButton();
+				}				
+			}		
+		}
+	}
+}
