b6667e20325c55f5da292665f910cd61475e527d
ArchangeL <dhunter24091992@gmail.com>
2013-11-19 19:51:10 +0200

    finished work with controls for PC

--- Assets/Scripts/Game/MoveMouth.cs
+++ Assets/Scripts/Game/MoveMouth.cs
@@ -1,20 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Scripts.Game
 {
     public class MoveMouth : MonoBehaviour
     {
         public GameObject[] SpawnPoints;
+        public bool IsOsWindows = false;
         private Vector3 startPosition;
 
         private void Start()
         {
             startPosition = transform.position;
+            SetIsOsWindows();
         }
 
         private void LateUpdate()
         {
-            MoveEater();
+            InputController();
         }
 
         private void MoveEater()
@@ -42,15 +45,68 @@ namespace Assets.Scripts.Game
                                 transform.position = new Vector3(transform.position.x, SpawnPoints[1].transform.position.y, transform.position.z);
                                 break;
                             default:
-                                transform.position = startPosition;
+                                transform.position = transform.position;
                                 break;
                         }
                     }
                 }
-                else
+                else if (touch.phase == TouchPhase.Ended)
                 {
                     transform.position = startPosition;
                 }
+            }
+        }
+
+        private void MoveEaterWIn()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "top" || hit.collider.tag == "center" || hit.collider.tag == "bottom"))
+                {
+                    string tag = hit.collider.gameObject.tag;
+                    switch (tag)
+                    {
+                        case "bottom":
+                            transform.position = new Vector3(transform.position.x, SpawnPoints[2].transform.position.y, transform.position.z);
+                            break;
+                        case "top":
+                            transform.position = new Vector3(transform.position.x, SpawnPoints[0].transform.position.y, transform.position.z);
+                            break;
+                        case "center":
+                            transform.position = new Vector3(transform.position.x, SpawnPoints[1].transform.position.y, transform.position.z);
+                            break;
+                        default:
+                            transform.position = transform.position;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void InputController()
+        {
+            if (IsOsWindows)
+            {
+                MoveEaterWIn();
+            }
+            else
+            {
+                MoveEater();
+            }
+        }
+
+        private void SetIsOsWindows()
+        {
+            string platform = Environment.OSVersion.Platform.ToString();
+            if (platform.Contains("Win"))
+            {
+                IsOsWindows = true;
+            }
+            else
+            {
+                IsOsWindows = false;
             }
         }
     }
