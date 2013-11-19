acfd77e884aecdc7df808c66dca921b3d5406670
frommars1992 <voronov1992@gmail.com>
2013-11-19 20:42:44 +0200

    Added_LoseMenu_&_EndGame

---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Game
+{
+		public class Game : MonoBehaviour
+		{
+
+				public static bool IsPause = false;
+				public static bool IsLose = false;
+
+				public GameMenuButtonsController Controller;
+				// Use this for initialization
+				void  Start ()
+				{
+		
+				}
+		
+				// Update is called once per frame
+				void Update ()
+				{
+		
+				}
+				public void CheckPause ()
+				{
+						if (Input.GetKeyUp (KeyCode.Escape)) {
+								if (!Controller._isGameMenuActive) {
+										Controller.PushGameMenuButton ();
+								} else {
+										Controller.PushResumeButton ();
+								}
+						}
+				}
+				
+				public void DisableTocuhColliders()
+				{
+				
+				}
+
+		}
+}
---
+++ Assets/Scripts/Game/LoseMenu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Game
+{
+		public class LoseMenu : MonoBehaviour
+		{
+
+				GameObject Menu;
+				GameObject Restart;
+				GameObject Background;
+				// Use this for initialization
+				void Start ()
+				{
+						Menu = GameObject.Find ("Menu");
+						Restart = GameObject.Find ("Restart");	
+						Background = GameObject.Find("LoseMenuBackground");
+						HideLoseMenu();					
+				}
+		
+				// Update is called once per frame
+				void Update ()
+				{
+						CheckIsLose ();
+				}
+
+				private void HideLoseMenu()
+				{
+					Menu.collider.enabled = false;
+					Menu.GetComponentInChildren<UISprite> ().enabled = false;
+					Restart.collider.enabled = false;
+					Restart.GetComponentInChildren<UISprite> ().enabled = false;
+					Background.GetComponent<UISprite>().enabled = false;
+				}
+				
+				private void ShowLoseMenu()
+				{
+					Menu.collider.enabled = true;
+					Menu.GetComponentInChildren<UISprite> ().enabled = true;
+					Restart.collider.enabled = true;
+					Restart.GetComponentInChildren<UISprite> ().enabled = true;	
+					Background.GetComponent<UISprite>().enabled = true;
+        }
+
+				private void CheckIsLose ()
+				{
+						if (Game.IsLose) {
+							ShowLoseMenu();
+						}
+				}
+				private void PushRestart()
+				{					
+					HideLoseMenu();
+					Game.IsLose = false;
+					ScoreAndSatiety.Satiety = 50f;
+					ScoreAndSatiety.Scores = 0;
+					Time.timeScale = 1;
+					Application.LoadLevel(1);
+				}
+				
+		}
+}
